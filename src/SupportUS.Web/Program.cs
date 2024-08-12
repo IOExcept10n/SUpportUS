da890edbd994ab3e096180c2f29c7131bd8360b2
Ivan Olyanishin <56886020+IOExcept10n@users.noreply.github.com>
2024-08-12 12:34:13 +0500

    Added api sample

--- src/SupportUS.Web/Program.cs
+++ src/SupportUS.Web/Program.cs
@@ -1,6 +1,8 @@
-using Microsoft.AspNetCore.Routing.Patterns;
+using Microsoft.EntityFrameworkCore;
+using SupportUS.Web.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<AppDbContext>();
 var app = builder.Build();
 
 app.MapGet("/", async context =>
@@ -12,6 +14,27 @@ app.MapGet("/", async context =>
     {
         await response.WriteAsync($"<h4>[{item.Key}] = {item.Value}</h4>");
     }
+});
+
+// API
+
+// Tasks
+app.MapGet("/api/tasks/find-task-by-id", async (HttpContext context, Guid id) =>
+{
+    var db = app.Services.GetRequiredService<AppDbContext>();
+    var task = await db.Quests.FindAsync(id);
+    var result = new
+    {
+        Found = task != null,
+        Task = task
+    };
+    await context.Response.WriteAsJsonAsync(result);
+});
+
+// Reviews
+app.MapPost("/api/reviews/publish", async (HttpContext context, Guid author, string text, int rate) =>
+{
+
 });
 
 app.Run();
