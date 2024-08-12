9633b82fb9c4f187c06e4105d866dacb7212f4b8
kkkiraaa <karina.hmelyova@gmail.com>
2024-08-12 13:22:32 +0500

    добавление пустых классов
    добавление пустых классов

--- src/SupportUS.Web/Program.cs
+++ src/SupportUS.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SupportUS.Web.Data;
+using System.Net.Sockets;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<AppDbContext>();
@@ -31,10 +32,98 @@ app.MapGet("/api/tasks/find-task-by-id", async (HttpContext context, Guid id) =>
     await context.Response.WriteAsJsonAsync(result);
 });
 
-// Reviews
-app.MapPost("/api/reviews/publish", async (HttpContext context, Guid author, string text, int rate) =>
+//Create
+app.MapPost("/api/reviews/createTaskv", async (HttpContext context, string authorName, string description, int coins) =>
 {
 
 });
+
+//Update
+app.MapPost("/api/reviews/updateTask", async (HttpContext context, Guid taskId, string description, int coins) =>
+{
+
+});
+
+//Delete
+app.MapPost("/api/reviews/deleteTask", async (HttpContext context, Guid taskId) =>
+{
+
+});
+
+//Take
+app.MapPost("/api/reviews/takeTask", async (HttpContext context, Guid taskId, Guid workerId) =>
+{
+
+});
+
+//Cancel
+app.MapPost("/api/reviews/cancelTask", async (HttpContext context, Guid taskId, Guid personId) =>
+{
+
+});
+
+//Report
+app.MapPost("/api/reviews/reportTask", async (HttpContext context, Ticket persone, Guid personId) =>
+{
+
+});
+
+//Complete
+app.MapPost("/api/reviews/completeTask", async (HttpContext context, Guid taskId, Guid personId) =>
+{
+
+});
+
+// Reviews
+app.MapPost("/api/reviews/publishRewiew", async (HttpContext context, Guid author, string text, int rate) =>
+{
+
+});
+
+app.MapPost("/api/reviews/deleteRewiew", async (HttpContext context, Guid idPersone, Guid idReview) =>
+{
+
+});
+
+app.MapPost("/api/reviews/reportRewiew", async (HttpContext context, Guid idReport, Ticket ticketPersone) =>
+{
+
+});
+
+
+//Profile
+app.MapPost("/api/reviews/createProfile", async (HttpContext context, Guid idReport, Ticket ticketPersone) =>
+{
+
+});
+
+app.MapPost("/api/reviews/reportProfile", async (HttpContext context, Guid idAuthor, Guid idReporter, Ticket ticketPersone) =>
+{
+
+});
+
+//AdminPanel
+app.MapPost("/api/reviews/banAdminPanel", async (HttpContext context, Guid idPersone, Ticket ticketReasone) =>
+{
+
+});
+
+app.MapPost("/api/reviews/unBanAdminPanel", async (HttpContext context, Guid idPersone) =>
+{
+
+});
+
+app.MapPost("/api/reviews/setCoinsAdminPanel", async (HttpContext context, Guid idPersone, int coins) =>
+{
+
+});
+
+app.MapPost("/api/reviews/undoTaskAdminPanel", async (HttpContext context, Guid idTask) =>
+{
+
+});
+
+
+
 
 app.Run();
