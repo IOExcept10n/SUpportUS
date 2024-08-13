89a0489c7345575b14ba767689a083f908e3832a
Jojer <124523356+MaximOdincov@users.noreply.github.com>
2024-08-14 00:52:23 +0500

    JOPA

--- src/SupportUS.Web/Bot/BotMailingService.cs
+++ src/SupportUS.Web/Bot/BotMailingService.cs
@@ -1,6 +1,28 @@
-﻿namespace SupportUS.Web.Bot
+﻿using Telegram.Bot.Types;
+using Microsoft.VisualBasic;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using SupportUS.Web.Models;
+namespace SupportUS.Web.Bot
 {
     public class BotMailingService(BotService bot, WebApplication app) : TelegramBotServiceBase(bot, app)
     {
+        public async Task MailMessageText(string msgText)
+        {
+            ChatId chatId = "-1002158004156";
+            await Bot.Client.SendTextMessageAsync(chatId, msgText);
+        }
+
+        public async Task MailMessageQuest(Message msg, Quest quest)
+        {
+            var inlineMarkup = new InlineKeyboardMarkup()
+                .AddNewRow().AddButton("Взять задание ✔️", "Done")
+                .AddNewRow().AddButton("Скрыть задание ❌", "Canceled");
+            await Bot.Client.SendTextMessageAsync(msg.Chat, quest., replyMarkup: inlineMarkup);
+        }
     }
 }
