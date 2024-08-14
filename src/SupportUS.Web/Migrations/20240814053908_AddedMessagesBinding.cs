6462f7801d91e117a0e8ab21aeef031ed2e6f2e4
Ivan Olyanishin <56886020+IOExcept10n@users.noreply.github.com>
2024-08-14 10:39:54 +0500

    Updated migration

---
+++ src/SupportUS.Web/Migrations/20240814053908_AddedMessagesBinding.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SupportUS.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedMessagesBinding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "BotMessageId",
+                table: "Quests",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MailMessageId",
+                table: "Quests",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BotMessageId",
+                table: "Quests");
+
+            migrationBuilder.DropColumn(
+                name: "MailMessageId",
+                table: "Quests");
+        }
+    }
+}
